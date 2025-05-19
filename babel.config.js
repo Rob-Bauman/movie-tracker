b2ff46810c5b15446b020d234d429f532196e5ea
Rob Bauman <rbauman08@gmail.com>
2025-05-19 07:55:39 -0400

    Babel config

--- babel.config.js
+++ babel.config.js
@@ -8,6 +8,10 @@ module.exports = {
       "whitelist": null,
       "safe": false,
       "allowUndefined": true
-    }]
+    }],
+    ['@babel/plugin-transform-private-methods', { loose: true }], // Set loose to true
+    // If you use these plugins, add them with the same loose option:
+    ['@babel/plugin-transform-class-properties', { loose: true }],
+    ['@babel/plugin-transform-private-property-in-object', { loose: true }],
   ]
 };
